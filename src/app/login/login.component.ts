20e1d87236396164e20c25bc100537d62da5735f
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-21 16:23:22 +0300

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  userInput = {};
+  buttonDisabled = true;
+
+  username = '';
+  password = '';
+  submitted: boolean = false;
+  @Output('auth') authenticated = new EventEmitter<boolean>();
+  @Output() lang = new EventEmitter<string>();
+  errorMessage = null;
 
   constructor() { }
 
