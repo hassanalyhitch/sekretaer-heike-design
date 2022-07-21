2d8538d2aae07371e4c6954d563c7926e9f836cb
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-21 16:18:07 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core/lib/translate.service';
 
 @Component({
   selector: 'my-sekretaer',
@@ -7,4 +8,20 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   
+  authenticated: boolean = false;
+  lang: string = 'en';
+
+  constructor(private translate: TranslateService){
+
+    //Translator
+    translate.setDefaultLang('en');
+
+    // const browserLang = translate.getBrowserLang();
+    // translate.use(browserLang.match(/en|de/) ? 'de' : 'en');
+  }
+
+  setLang(lang: string){
+    this.lang = lang;
+    this.translate.use(lang);
+  }
 }
