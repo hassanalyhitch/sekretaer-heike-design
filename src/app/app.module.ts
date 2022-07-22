d028aefa2a768474b6375ac364f54803794e356d
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-22 18:15:15 +0300

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { SnackbarComponent } from './snackbar/snackbar.component';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { HomeComponent } from './dashboard/home/home.component';
+import { FavouriteComponent } from './dashboard/favourite/favourite.component';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(httpClient: HttpClient) {
@@ -52,7 +53,8 @@ const appRoutes: Routes = [
     LoginComponent,
     SnackbarComponent,
     DashboardComponent,
-    BottomNavComponent
+    BottomNavComponent,
+    FavouriteComponent
    ],
   bootstrap:    [ AppComponent ],
   providers: [
