13527374631c4fd5a330950a510b1ec8b35e6be0
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-27 13:02:56 +0300

    3 files modified

--- src/app/dashboard/contract-detail/contract-detail.component.ts
+++ src/app/dashboard/contract-detail/contract-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contract-detail',
@@ -9,13 +10,22 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class ContractDetailComponent implements OnInit {
 
   @Input() index:string;
+  hrTitle: string;
 
   constructor(
   private route: ActivatedRoute,
-  private router: Router ) { }
+  private router: Router, private translate:TranslateService ) {
+    this.hrTitle = this.translate.instant('insurance.detail.hrtitle');
+  
+  }
 
   ngOnInit() {
     this.index = this.route.snapshot.paramMap.get('id');
+
+    const wholeDocTemplate = document.getElementsByClassName('_card-content').item(0) as HTMLElement | null;
+    if (wholeDocTemplate != null) {
+      document.getElementById("docu-hr").setAttribute('data-content', this.hrTitle);
+    } 
   }
 
 }
