7018d9a4fc172759f1b2f118914f024673630e61
hassanalyhitch <hassanaly.b@gmail.com>
2022-08-22 16:07:08 +0300

    5 files modified

--- src/app/dashboard/notifications/notification-item/notification-item.component.ts
+++ src/app/dashboard/notifications/notification-item/notification-item.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationItemComponent implements OnInit {
 
+  notifMessage:string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
   constructor() { }
 
   ngOnInit() {
