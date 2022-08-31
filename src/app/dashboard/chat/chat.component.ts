f5d7146edbd58d9d347338fd1ac2085300f9ef3b
hassanalyhitch <hassanaly.b@gmail.com>
2022-08-31 16:38:35 +0300

    3 files modified

--- src/app/dashboard/chat/chat.component.ts
+++ src/app/dashboard/chat/chat.component.ts
@@ -10,6 +10,13 @@ export class ChatComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const text = document.querySelectorAll(".text");
+    let delay = 0;
+    text.forEach((el:any)=>{
+      el.style.animation = "fade-in 0.4s ease forwards";
+      el.style.animationDelay= delay +"s";
+      delay += 0.25;
+    });
   }
 
 }
