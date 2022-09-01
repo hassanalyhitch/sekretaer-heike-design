9d2259986b3af357a134809ab5b42bc75f87dc2f
hassanalyhitch <hassanaly.b@gmail.com>
2022-09-01 15:32:47 +0300

    3 files modified

--- src/app/dashboard/chat/chat.component.ts
+++ src/app/dashboard/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-chat',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+
+  @ViewChild('scrollChat', { static: true }) private scrollChat: ElementRef;
 
   constructor() { }
 
@@ -17,6 +19,16 @@ export class ChatComponent implements OnInit {
       el.style.animationDelay= delay +"s";
       delay += 0.25;
     });
+    this.scrollToBottom();
   }
 
+
+  scrollToBottom(): void {
+    try {
+        document.getElementById('scrollChat').scrollTop = document.getElementById('scrollChat').scrollHeight;
+        // this.scrollChat.nativeElement.scrollTop = this.scrollChat.nativeElement.scrollHeight;
+    } catch(err) { 
+      console.log(err)
+    }                 
+  }
 }
