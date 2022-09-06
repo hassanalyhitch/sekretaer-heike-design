96eb9f460cd15a9e02c48bafd2cdfe1141ad8cc0
hassanalyhitch <hassanaly.b@gmail.com>
2022-09-06 13:52:54 +0300

    2 files modified

--- src/app/dashboard/home/add-page/add-page.component.ts
+++ src/app/dashboard/home/add-page/add-page.component.ts
@@ -5,7 +5,6 @@ import { Subscription } from 'rxjs';
 import { FolderData } from '../../../models/folder.model';
 import { FoldersService } from '../../../services/folder.service';
 import { ContractsService } from '../../../services/contracts.service';
-import { Uploader } from '@syncfusion/ej2-inputs';
 
 
 @Component({
