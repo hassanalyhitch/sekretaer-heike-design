6738c4cf7beea2cefd4f124dd87cfaabf9e6d1aa
hassanalyhitch <hassanaly.b@gmail.com>
2022-09-22 15:23:40 +0300

    2 files modified

--- src/app/dashboard/folder-detail/folder-detail.component.ts
+++ src/app/dashboard/folder-detail/folder-detail.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router, Event, ActivatedRoute } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 import { FolderData } from '../../models/folder.model';
 import { FoldersService } from '../../services/folder.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-folder-detail',
   templateUrl: './folder-detail.component.html',
   styleUrls: ['./folder-detail.component.css']
 })
-export class FolderDetailComponent implements OnInit {
+export class FolderDetailComponent implements OnInit, OnDestroy {
 
   folder: FolderData;
   hrTitle: string;
@@ -17,8 +19,13 @@ export class FolderDetailComponent implements OnInit {
 
   parentNav: string = '';
   currentNav: string = '';
+  mainFolder: FolderData;
+  parentFolder: FolderData;
+  hasParent: boolean = false;
+  routeListener: Subscription;
+  visitedFolderArray:FolderData[] = [];
 
-  constructor(private router:Router, private folderService: FoldersService, private translate:TranslateService) { }
+  constructor(private router:Router, private route:ActivatedRoute, private folderService: FoldersService, private translate:TranslateService, private _location: Location) { }
 
   ngOnInit() {
     console.log(this.folderService.selectedFolder);
@@ -31,17 +38,63 @@ export class FolderDetailComponent implements OnInit {
       document.getElementById("docu-hr").setAttribute('data-content', this.hrTitle);
     } 
 
+    this.routeListener = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        
+        this.folder = this.folderService.selectedFolder;
+
+        if(this.visitedFolderArray.length>0){
+          for(let i=0; i<this.visitedFolderArray.length; i++){
+            
+            if(this.visitedFolderArray[i].id == this.route.snapshot.params['id']){
+              
+              this.folderService.emitSelectedFolder(this.visitedFolderArray[i]);
+              this.folder = this.folderService.selectedFolder;
+
+              for(let x=0; x<this.visitedFolderArray.length; x++){
+                if(this.visitedFolderArray[x].id == this.folder.ownerFolderId){
+                  this.parentNav = this.visitedFolderArray[x].folderName;
+                }
+              }
+            }
+          }
+        } 
+        if(this.mainFolder.id == this.route.snapshot.params['id']){
+          this.parentNav = '';
+          this.folder = this.mainFolder;
+        }
+        this.currentNav = this.folder.folderName;
+      }
+    });
+
     this.folder = this.folderService.selectedFolder;
     this.currentNav = this.folder.folderName;
+
+    //main folder set once and shouldn't change
+    this.mainFolder = this.folder; 
+    this.visitedFolderArray.push(this.mainFolder);
+
+  }
+
+  ngOnDestroy(){
+    this.hasParent = false;
+    console.log('hasParent -> '+this.hasParent);
+    this.routeListener.unsubscribe();
+    this.visitedFolderArray = [];
   }
   
-  onFolderCardClick(clickedFolder: FolderData){
-    console.log("open - > "+clickedFolder.id+" "+clickedFolder.folderName);
+  onFolderCardClick(clickedFolder: FolderData, index){
     this.folderService.emitSelectedFolder(clickedFolder);
-    this.parentNav = this.currentNav;
-    this.currentNav = clickedFolder.folderName;
+
+    this.visitedFolderArray.push(clickedFolder);
+    this.parentFolder = this.folder;
+    this.hasParent = true;
+
+
     this.router.navigate(['/dashboard/overview/folder-detail', { id: clickedFolder.id }]);
-    this.folder = this.folderService.selectedFolder;
+  }
+  onBackNavClick(){
+    this._location.back();
   }
 
 }
