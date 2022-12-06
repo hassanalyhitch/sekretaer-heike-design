44947ae7294074cb8bef6f1716d764c3cfde57cb
hassanalyhitch <hassanaly.b@gmail.com>
2022-12-06 17:17:36 +0300

    6 files modified

--- src/app/dashboard/fav-item/fav-item.component.ts
+++ src/app/dashboard/fav-item/fav-item.component.ts
@@ -4,7 +4,7 @@ import { ContractData } from '../../models/contract.model';
 @Component({
   selector: 'app-fav-item',
   templateUrl: './fav-item.component.html',
-  styleUrls: ['./fav-item.component.scss']
+  styleUrls: ['./fav-item.component.css']
 })
 export class FavItemComponent implements OnInit {
 
